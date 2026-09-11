396213f0d85d78f78b6876ac0bd81147a349a305
prathvirajkodachadri <133985360+prathvirajkodachadri@users.noreply.github.com>
2026-09-11 16:37:14 +0530

    Delete AI_MIX_ANALYZER directory

--- AI_MIX_ANALYZER/tests/test_sections.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from analyzer.audio_loader import AudioData
-from analyzer.sections import analyze_sections
-
-def test_sections():
- a=AudioData(np.zeros(44100*21,dtype=np.float32),44100,'test.wav')
- r=analyze_sections(a)
- assert len(r['sections'])==2
- assert r['dynamic_contrast_db']==0
